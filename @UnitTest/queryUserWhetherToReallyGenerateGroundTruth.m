0943ae85ecb664af42e88770012655e18e2900bd
np_cottaris <cottaris@sas.upenn.edu>
2015-02-27 14:24:55 -0500

    Improved query dialog for ground truth data.
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- @UnitTest/queryUserWhetherToReallyGenerateGroundTruth.m
+++ @UnitTest/queryUserWhetherToReallyGenerateGroundTruth.m
@@ -2,56 +2,49 @@
 % (only evoked if the validation data set is not found)
 function forceGenerateGroundTruth = queryUserWhetherToReallyGenerateGroundTruth(obj, validationMode, scriptName)
 
-    generateString = upper('Yes, I know what I am doing!');
-    donotGenerateString = upper('Ooops... No, please do not generate the ground truth data.');
+    generateDataForThisScript = upper('Yes, for this script *only*.');
+    generateDataForAllScripts = upper('Yes, for this *and* any other scripts with missing data.');
+    donotGenerateData         = upper('No, I will email the ISETBIO team for the ground truth data.');
     
-    % Construct a questdlg with three options
-    questionString = sprintf('Generate ''%s'' ground truth data set for this script?', validationMode);
+    % Construct a questdlg with four options
+    questionString = sprintf('Generate ''%s'' ground truth data set(s)?', validationMode);
     headerString  = sprintf('ALERT: A ''%s'' ground truth data set for script ''%s'' was not found!', validationMode, scriptName);
     choice = questdlg(upper(questionString), ...
                       upper(headerString), ...
-                      generateString, donotGenerateString, donotGenerateString);
+                      generateDataForThisScript, generateDataForAllScripts, donotGenerateData, donotGenerateData);
                   
     % Handle response
     switch choice
-        case generateString
+        case generateDataForThisScript
             forceGenerateGroundTruth = true;
-        
-        case donotGenerateString
+            if (strcmp(validationMode, 'FAST'))
+                obj.forceGenerateFastGroundTruthForAllScripts = false;
+            end
+            if (strcmp(validationMode, 'FULL'))
+                obj.forceGenerateFullGroundTruthForAllScripts = false;
+            end
+            
+        case generateDataForAllScripts
+            forceGenerateGroundTruth = true;
+            if (strcmp(validationMode, 'FAST'))
+                obj.forceGenerateFastGroundTruthForAllScripts = true;
+            end
+            if (strcmp(validationMode, 'FULL'))
+                obj.forceGenerateFullGroundTruthForAllScripts = true;
+            end
+            
+        case donotGenerateData
             forceGenerateGroundTruth = false;
-    end
-    
-    
-    if (forceGenerateGroundTruth)
-        repeatForAllString = upper('Yes, generate ground truth for all remaining scripts and don''t ask me again!');
-        doNotRepeatForAllString = upper('No, ask me separately for every script.');
-    
-        % Construct a questdlg with three options
-        headerString = sprintf('Generate ''%s'' ground truth data for all remaining scripts that do not have a ground truth ?', validationMode);
-        choice = questdlg('', ...
-                          upper(headerString), ...
-                          repeatForAllString, doNotRepeatForAllString, doNotRepeatForAllString);
-                      
-        % Handle response
-        switch choice
-            case repeatForAllString
-                if (strcmp(validationMode, 'FAST'))
-                    obj.forceGenerateFastGroundTruthForAllScripts = true;
-                end
-                if (strcmp(validationMode, 'FULL'))
-                    obj.forceGenerateFullGroundTruthForAllScripts = true;
-                end
-
-            case doNotRepeatForAllString
-                if (strcmp(validationMode, 'FAST'))
-                    obj.forceGenerateFastGroundTruthForAllScripts = false;
-                end
-                if (strcmp(validationMode, 'FULL'))
-                    obj.forceGenerateFullGroundTruthForAllScripts = false;
-                end
-                
-        end
-    
+            if (strcmp(validationMode, 'FAST'))
+                obj.forceGenerateFastGroundTruthForAllScripts = false;
+            end
+            if (strcmp(validationMode, 'FULL'))
+                obj.forceGenerateFullGroundTruthForAllScripts = false;
+            end
+            
+            h = warndlg(upper('Validation data sets are available by the ISETBIO team.'),...
+                upper('email cottaris@sas.upenn.edu '));
+            uiwait(h);
     end
     
 end
