14efef181a9c285542256a7d595f1255eaf70796
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-08-08 16:51:24 +0300

    Now printing the Diff value.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- @UnitTest/assertIsZero.m
+++ @UnitTest/assertIsZero.m
@@ -2,8 +2,8 @@
 function assertIsZero(diffExpression,exprString,tolerance)
     
     %% Do the comparison
-    if (max(abs(diffExpression) > tolerance))
-        message = sprintf('%s differs from zero by more than tolerance (%0.1g).', exprString, tolerance);
+    if (max(abs(diffExpression(:)) > tolerance))
+        message = sprintf('%s differs from zero by more than tolerance (%0.1g). [Diff: %g]', exprString, tolerance, max(abs(diffExpression(:))));
         UnitTest.validationRecord('FAILED', message);
     else
         message = sprintf('%s is within the specified tolerance (%0.1g) of zero.', exprString, tolerance);
