c9478b43aa7c80d123a65b2e1a001f4ef1d48a92
Ben Heasly <benjamin.heasly@gmail.com>
2017-01-24 12:51:07 -0500

    summaryReport flags reflect  runtime or validation failure

--- @UnitTest/validate.m
+++ @UnitTest/validate.m
@@ -464,8 +464,8 @@ function abortValidationSession = validate(obj, vScriptsToRunList)
             summaryReportEntry.textIsBold{5} = false;
         end
        
-        summaryReportEntry.fastFailed = strcmp(resultStingFastValidation, 'FAILED');
-        summaryReportEntry.fullFailed = strcmp(resultStingFullValidation, 'FAILED');
+        summaryReportEntry.fastFailed = exceptionRaisedFlag || validationFailedFlag || strcmp(resultStingFastValidation, 'FAILED');
+        summaryReportEntry.fullFailed = exceptionRaisedFlag || validationFailedFlag || strcmp(resultStingFullValidation, 'FAILED');
         summaryReportEntry.timeLapsed = timeLapsed; 
         obj.summaryReport{numel(obj.summaryReport)+1} = summaryReportEntry;
     end % scriptIndex
